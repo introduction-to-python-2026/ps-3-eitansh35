0c7313fd61f4cd32eb8ef1fe3c26440d8610ccfa
eitansh35 <eitansh35@gmail.com>
2025-11-06 18:23:40 +0200

    Fix indentation in edge_case_exercise.py

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -6,13 +6,13 @@ def move(my_list, direction):
     # Move the one to the left or to the right
     if direction == 'right':
     if index_of_one < list_length - 1:
-      my_list[index_of_one] = 0
-      my_list[index_of_one + 1] = 1
+    my_list[index_of_one] = 0
+    my_list[index_of_one + 1] = 1
 
     elif direction == 'left':
     if index_of_one > 0:
-      my_list[index_of_one] = 0
-      my_list[index_of_one - 1] = 1
+    my_list[index_of_one] = 0
+    my_list[index_of_one - 1] = 1
 
     return my_list
 
