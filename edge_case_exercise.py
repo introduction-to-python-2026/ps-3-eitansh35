c53937b2213c799c8c860801b14d98b7ea1dcb4d
eitansh35 <eitansh35@gmail.com>
2025-11-06 13:15:00 +0200

    Define list_length to prevent index errors
    Fix index out of range error by defining list_length.

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -2,13 +2,16 @@ def move(my_list, direction):
 
     # Finds the index of the one in the list
     index_of_one = my_list.index(1)
-
+list_length=len(my_list)
+    
     # Move the one to the left or to the right
     if direction == 'right':
+    if index_of_one<list length-1:
         my_list[index_of_one] = 0
         my_list[index_of_one + 1] = 1
 
     elif direction == 'left':
+    if index_of_one > 0:
         my_list[index_of_one] = 0
         my_list[index_of_one - 1] = 1
 
